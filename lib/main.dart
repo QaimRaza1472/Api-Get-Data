54d4ce8164ddcc7c14dfe87e89513f9ea5f71b52
QaimRaza1472 <qaimgee572@gmail.com>
2022-03-01 16:38:45 +0500

    My Commit

--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
+      title: 'Api Calling',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
